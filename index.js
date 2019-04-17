de3250a6b89beb0a03df312243c64209837bff84
NikoRozo <niko.rozo92@outlook.com>
2019-04-17 11:31:21 -0500

    Ejecución de comandos en la Terminal

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const cp = require('child_process');
+
+function execCommand(command) {
+    cp.exec(command, (err, stdout, stderr) => {
+        if (err) {
+            console.log(`Error: ${err}`); return;
+        }
+        if (stdout) console.log(`Standard Out: \n ${stdout}`);
+        if (stderr) console.log(`Standard Out: \n ${stdout}`);
+    });
+}
+
+execCommand('node ./src/script --base=10');
