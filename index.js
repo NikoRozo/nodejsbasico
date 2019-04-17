43417cca13722e583eb9015b20ee866cf95e955a
NikoRozo <niko.rozo92@outlook.com>
2019-04-17 11:15:48 -0500

    Leer Datos por Consola y Capturar Datos

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const q = [
+    'Cual es tu Primer Nombre?: ',
+    'Cual es tu Primer Apellido?: ',
+    'Cual es tu edad?: '
+]
+
+const AskQuestion = (rl, question) => {
+    return new Promise((resolve, rej) => {
+        rl.question(question, answer => {
+            resolve(answer);
+        });
+    });
+}
+
+const Ask = function (questions) {
+    return new Promise(async resolve => {
+        let results = [];
+        for (let i = 0; i < questions.length; i++) {
+            const result = await AskQuestion(rl, questions[i]);
+            results.push(result);
+        }
+        rl.close;
+        resolve(results);
+    });
+}
+
+Ask(q).then(q => {
+    console.log(`Hola ${q[0]} ${q[1]}, tu edad es la siguiente ${q[2]}`);
+});
