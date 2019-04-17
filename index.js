c8dd6df85ad1ee8ff1a13563901ba7321ce341d1
NikoRozo <niko.rozo92@outlook.com>
2019-04-17 13:05:11 -0500

    Iniciar servidor Express

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Estoy en la ruta Home');
+});
+
+app.listen(3000, () => {
+    console.log('Servidor Express esta Iniciado en el puerto 3000');
+});
