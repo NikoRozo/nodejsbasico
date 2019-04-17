3c33485884d3cb9ca635a80e216c10e6972209b1
NikoRozo <niko.rozo92@outlook.com>
2019-04-17 10:55:16 -0500

    Leer y Escribir Archivos

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const fs = require('fs');
+const ops =  require('./src/fileops');
+
+let incValue;
+
+fs.readFile('./resources/number.txt', "utf8", (error, text) => {
+    if (error) throw error;
+
+    const number = text.split("\n").map( n => Number(n));
+    incValue = ops.incrementValues(number);
+    fs.writeFile('./resources/numbernew.txt', incValue.join('\n'), (error, result) => {
+        if (error) throw error;
+    });
+});
